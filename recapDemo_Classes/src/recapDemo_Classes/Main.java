2d7d5151e84e9986c93171e84d07f827c2af4669
Atilla <63123956+atillakalay@users.noreply.github.com>
2022-03-06 12:37:07 +0300

    3. Gün Ödev 3 tamamlandı.

--- recapDemo_Classes/src/recapDemo_Classes/Main.java
+++ recapDemo_Classes/src/recapDemo_Classes/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		DortIslem dortIslem = new DortIslem();
-		System.out.println(dortIslem.cikar(10, 5, 3));
+		System.out.println(dortIslem.cikar(10, 5));
 	}
 
 }

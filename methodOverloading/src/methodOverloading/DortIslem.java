ea9fe10b28b00e00121546ecfeaa85cc4898f545
Atilla <63123956+atillakalay@users.noreply.github.com>
2022-03-06 13:12:28 +0300

    3. Gün Ödev 3 tamamlandı.

---
+++ methodOverloading/src/methodOverloading/DortIslem.java
@@ -0,0 +1,12 @@
+package methodOverloading;
+
+public class DortIslem {
+	public void topla(int sayi1, int sayi2) {
+		System.out.println(sayi1 + sayi2);
+	}
+
+	public void topla(int sayi1, int sayi2, int sayi3) {
+		System.out.println(sayi1 + sayi2 + sayi3);
+	}
+
+}
---
+++ methodOverloading/src/methodOverloading/Main.java
@@ -0,0 +1,11 @@
+package methodOverloading;
+
+public class Main {
+
+	public static void main(String[] args) {
+		DortIslem dortIslem = new DortIslem();
+		dortIslem.topla(1, 2, 3);
+
+	}
+
+}

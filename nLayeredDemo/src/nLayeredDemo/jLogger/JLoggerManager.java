705d8b17de7592a067b4b0f79fde6abfb842d514
Atilla <63123956+atillakalay@users.noreply.github.com>
2022-03-15 21:39:05 +0300

    5. Gün Kamp tamamlandı.

--- nLayeredDemo/src/nLayeredDemo/jLogger/JLoggerManager.java
+++ nLayeredDemo/src/nLayeredDemo/jLogger/JLoggerManager.java
@@ -2,6 +2,6 @@ package nLayeredDemo.jLogger;
 
 public class JLoggerManager {
 	public void Log(String message) {
-		System.out.println("J Logger ile loglandý!");
+		System.out.println("J Logger ile loglandý: " + message);
 	}
 }
